bfda919ee3a3ff2d14107176018324d708d92b3e
Dmitrii Gorodkov <dgorodkov@gmail.com>
2016-06-14 14:14:51 +0300

    Hexlet solution added.

--- src/exercises/ifInt/ClassGen.java
+++ src/exercises/ifInt/ClassGen.java
@@ -146,4 +146,43 @@ mv.visitLabel(elseLable);
 mv.visitJumpInsn(Opcodes.IF_ICMPLE, elseLable);
 Данный код добавляет инструкцию IF_ICMPLE с меткой "elseLable".
 
+
+Hexlet solution
+
+// BEGIN
+        mv.visitVarInsn(Opcodes.ILOAD, 0);
+        mv.visitVarInsn(Opcodes.ILOAD, 1);
+        mv.visitJumpInsn(Opcodes.IF_ICMPGE, elseLabel);
+        mv.visitVarInsn(Opcodes.ILOAD, 0);
+        mv.visitInsn(Opcodes.IRETURN);
+        mv.visitLabel(elseLabel);
+        mv.visitVarInsn(Opcodes.ILOAD, 1);
+        mv.visitInsn(Opcodes.IRETURN);
+        mv.visitMaxs(2, 2);
+// END
+
+// BEGIN
+        // comparing local variable 0 with 1 and saving min to the 0
+        mv.visitVarInsn(Opcodes.ILOAD, 0);
+        mv.visitVarInsn(Opcodes.ILOAD, 1);
+        mv.visitJumpInsn(Opcodes.IF_ICMPLE, elseLabel);
+        mv.visitVarInsn(Opcodes.ILOAD, 1);
+        mv.visitVarInsn(Opcodes.ISTORE, 0);
+        mv.visitLabel(elseLabel);
+
+        // comparing local variable 0 with 2 and saving min to the 0
+        mv.visitVarInsn(Opcodes.ILOAD, 0);
+        mv.visitVarInsn(Opcodes.ILOAD, 2);
+        mv.visitJumpInsn(Opcodes.IF_ICMPLE, elseLabel2);
+        mv.visitVarInsn(Opcodes.ILOAD, 2);
+        mv.visitVarInsn(Opcodes.ISTORE, 0);
+        mv.visitLabel(elseLabel2);
+
+        // returning local variable 0 (since it's the minimum)
+        mv.visitVarInsn(Opcodes.ILOAD, 0);
+        mv.visitInsn(Opcodes.IRETURN);
+        mv.visitMaxs(2, 2);
+// END
+
+
 */
