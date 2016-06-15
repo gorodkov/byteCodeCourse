be8b51536d49b6ae21db6c20a0f6ebf0128e60c0
Dmitrii Gorodkov <dgorodkov@gmail.com>
2016-06-15 12:24:14 +0300

    Hexlet solution added.

--- src/exercises/forLoop/ClassGen.java
+++ src/exercises/forLoop/ClassGen.java
@@ -100,4 +100,43 @@ public static int sum(final int a) {
 Собственно и все =) Остальное Вам уже должно быт известно. Задание не из легких и колличество байт-кода может
 быть больше чем в прошлых работах, но мы верим в то что все получиться. До встречи в следующем уроке!
 
- */
+Hexlet solution:
+
+// BEGIN
+        // int result = 0;
+        mv.visitInsn(Opcodes.ICONST_0);
+        mv.visitVarInsn(Opcodes.ISTORE, 1);
+
+        // int i = 0;
+        mv.visitInsn(Opcodes.ICONST_0);
+        mv.visitVarInsn(Opcodes.ISTORE, 2);
+
+        // i <= a;
+        mv.visitLabel(forLabel);
+        mv.visitVarInsn(Opcodes.ILOAD, 2);
+        mv.visitVarInsn(Opcodes.ILOAD, 0);
+        mv.visitJumpInsn(Opcodes.IF_ICMPGE, label);
+
+
+        mv.visitVarInsn(Opcodes.ILOAD, 1);
+        mv.visitVarInsn(Opcodes.ILOAD, 2);
+        mv.visitInsn(Opcodes.ICONST_1);
+        // (i + 1)
+        mv.visitInsn(Opcodes.IADD);
+        // result + (i + 1)
+        mv.visitInsn(Opcodes.IADD);
+        // result = result + (i + 1);
+        mv.visitVarInsn(Opcodes.ISTORE, 1);
+
+        // i++;
+        mv.visitIincInsn(2, 1);
+        mv.visitJumpInsn(Opcodes.GOTO, forLabel);
+
+        mv.visitLabel(label);
+        mv.visitVarInsn(Opcodes.ILOAD, 1);
+        mv.visitInsn(Opcodes.IRETURN);
+        mv.visitMaxs(3, 3);
+        // END
+
+
+*/
